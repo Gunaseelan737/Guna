7ef6348432d7a2fd6696ef631db720a116c7935c
Gunaseelan737 <innacigunaseelan59@gmail.com>
2022-02-06 15:19:19 +0530

    hello

---
+++ src/org/tcm/Tcm.java
@@ -0,0 +1,12 @@
+package org.tcm;
+
+public class Tcm {
+	public void add() {
+System.out.println("addition");
+	}
+	public static void main(String[] args) {
+		Tcm t =new Tcm();
+		t.add();
+	}
+
+}
